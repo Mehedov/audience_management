e9f942bdbc14c76136b6b1e0bd9fd8532cdd454c
Mehedov <mehedov.dev@yandex.ru>
2025-02-14 15:50:38 +0300

    Moved the logic of the Control component to the ControlProblemMessage component

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -1,60 +1,13 @@
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { IComputerData } from '@/redux/slices/computer/slice'
 import { useRef, useState } from 'react'
 import { ControlAudienceSearch } from './ControlAudienceSearch'
 import { ControlProblemMessage } from './ControlProblemMessage'
 
-type SearchTabsProps = {
-    computers: IComputerData[]
-}
-
-type TProblemMessage = {
-    id: string
-    author: string
-    text: string
-}
-
-export type TSearchTabsForm = {
-    problemMessage: TProblemMessage
-    setProblemMessage: React.Dispatch<React.SetStateAction<TProblemMessage>>
-    isError: boolean
-    setIsError: React.Dispatch<React.SetStateAction<boolean>>
-    setIsSuccess: React.Dispatch<React.SetStateAction<boolean>>
-    isSuccess: boolean
-    validateProblemMessage: () => NodeJS.Timeout
-}
-
-export function Control({ computers }: SearchTabsProps) {
-    const [problemMessage, setProblemMessage] = useState<TProblemMessage>({
-        id: '',
-        author: '',
-        text: '',
-    })
+export function Control() {
     const [isError, setIsError] = useState(false)
     const [isSuccess, setIsSuccess] = useState(false)
     const textError = useRef('Все поля должны быть заполнены.')
     const textSuccess = useRef('Ваше сообщение отправлено.')
-
-    const validateProblemMessage = () => {
-        const thereIdComputer = computers.find(
-            (obj) => obj.id === problemMessage.id
-        )
-
-        const isEmptyField =
-            !problemMessage.id || !problemMessage.author || !problemMessage.text
-
-        if (isEmptyField) {
-            setIsError(true)
-            textError.current = 'Все поля должны быть заполнены.'
-        } else if (!thereIdComputer) {
-            textError.current = 'Номер компьютера не найден'
-            setIsError(true)
-        }
-
-        return setTimeout(() => {
-            setIsError(false)
-        }, 2000)
-    }
 
     return (
         <Tabs defaultValue="Аудитория" className="max-[600">
@@ -64,15 +17,12 @@ export function Control({ computers }: SearchTabsProps) {
             </TabsList>
             <ControlAudienceSearch />
             <ControlProblemMessage
-                problemMessage={problemMessage}
-                setProblemMessage={setProblemMessage}
                 isError={isError}
                 setIsError={setIsError}
                 setIsSuccess={setIsSuccess}
                 isSuccess={isSuccess}
-                validateProblemMessage={validateProblemMessage}
-                textError={textError.current}
-                textSuccess={textSuccess.current}
+                textError={textError}
+                textSuccess={textSuccess}
             />
         </Tabs>
     )
