adf6eaa943174cbdd4230cecd96da499b8b4b54a
Mehedov <mehedov.dev@yandex.ru>
2025-02-16 16:24:25 +0300

    Added error handling

--- src/utils/find.utils.ts
+++ src/utils/find.utils.ts
@@ -3,5 +3,7 @@ import { IComputerData } from '@/redux/slices/computer/slice'
 export const findComputerById = (id: string, computers: IComputerData[]) => {
     if (id && computers) {
         return computers.find((comp) => comp.id === id)
+    } else {
+        throw new Error('Компьютер не найден')
     }
 }
