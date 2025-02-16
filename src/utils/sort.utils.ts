b9cd430d11e57a56ddc46ff41cc83bda31defc29
Mehedov <mehedov.dev@yandex.ru>
2025-02-16 16:27:01 +0300

    sort.utils.ts: Added error handling

--- src/utils/sort.utils.ts
+++ src/utils/sort.utils.ts
@@ -1,16 +1,20 @@
 import { IComputerData } from '@/redux/slices/computer/slice'
 
 export const sortByAuditories = (item: IComputerData[], order?: string) => {
-    const sortedItem = [...item].sort((a, b) => {
-        if (
-            typeof a.auditorium !== 'number' ||
-            typeof b.auditorium !== 'number'
-        ) {
-            throw new Error('Invalid auditorium value')
-        }
-        return order === 'desc'
-            ? b.auditorium - a.auditorium
-            : a.auditorium - b.auditorium
-    })
-    return sortedItem
+    if (item) {
+        const sortedItem = [...item].sort((a, b) => {
+            if (
+                typeof a.auditorium !== 'number' ||
+                typeof b.auditorium !== 'number'
+            ) {
+                throw new Error('Invalid auditorium value')
+            }
+            return order === 'desc'
+                ? b.auditorium - a.auditorium
+                : a.auditorium - b.auditorium
+        })
+        return sortedItem
+    } else {
+        throw new Error('В качестве аргумента не было добавлен item')
+    }
 }
