e4d226ae1d11ee8c8e2cd659ca4071e7f815bd54
Mehedov <mehedov.dev@yandex.ru>
2025-02-18 12:48:24 +0300

    I made a full-width table

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,18 +1,20 @@
 import { AddingComputer } from '@/components/Admin/AddingComputer'
 import { DataProblems } from '@/components/Admin/DataProblems'
+import { DeleteComputer } from '@/components/Admin/DeleteComputer'
 import { Component } from '@/components/CountProblem/CountProblem'
-import {DeleteComputer} from '@/components/Admin/DeleteComputer'
 
 const Admin = () => {
     return (
         <div className="max-w-[1200px] m-auto p-5 space-y-5">
-            <div className="flex w-full items-center gap-10 flex-col md:flex-row justify-between space-x-5">
+            <div className="flex justify-center">
                 <DataProblems />
-                <Component />
             </div>
-            <div className='flex gap-10 items-center'>
+            <div className="flex w-full items-center gap-5 flex-col md:flex-row justify-between space-x-5">
+                <DeleteComputer />
                 <AddingComputer />
-                <DeleteComputer/>
+            </div>
+            <div className="flex gap-10 items-center">
+                <Component />
             </div>
         </div>
     )
