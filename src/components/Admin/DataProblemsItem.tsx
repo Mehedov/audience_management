2cfc167fe41d33ac5253afc88208601a0ef2ea4a
Mehedov <mehedov.dev@yandex.ru>
2025-02-18 15:24:16 +0300

    Changed the width

--- src/components/Admin/DataProblemsItem.tsx
+++ src/components/Admin/DataProblemsItem.tsx
@@ -12,12 +12,12 @@ const DataProblemsItem: FC<TDataProblemsItemProps> = ({
 }) => {
     return (
         <div className="flex items-center gap-5 dark:border-neutral-700 border-b-[1px] p-3 ">
-            <div className="w-[15%] flex items-center gap-1">
+            <div className="w-[18%] flex items-center gap-1">
                 <div>№{id}</div>
                 <span>ауд. {auditorium}</span>
             </div>
             <div className="w-[60%] break-words">{message}</div>
-            <div className="w-[15%]">
+            <div className="w-[12%]">
                 <ChangeStatusSelect status={status} />
             </div>
         </div>
