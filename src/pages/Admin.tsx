de7ae8dd940536289b37b6534fc8c0a680bfedd8
Mehedov <mehedov.dev@yandex.ru>
2025-02-18 15:22:12 +0300

    Adding the BottomDataPanel component

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,20 +1,16 @@
 import { AddingComputer } from '@/components/Admin/AddingComputer'
-import { DataProblems } from '@/components/Admin/DataProblems'
 import { DeleteComputer } from '@/components/Admin/DeleteComputer'
-import { Component } from '@/components/CountProblem/CountProblem'
+import { BottomDataPanel } from '@/components/RightDataPanel'
 
 const Admin = () => {
     return (
-        <div className="max-w-[1200px] m-auto p-5 space-y-5">
+        <div className="max-w-[1200px] h-auto m-auto p-5 space-y-5 overflow-y-hidden scrollable-container">
             <div className="flex justify-center">
-                <DataProblems />
+                <BottomDataPanel />
             </div>
             <div className="flex w-full items-center gap-5 flex-col md:flex-row justify-between space-x-5">
-                <DeleteComputer />
                 <AddingComputer />
-            </div>
-            <div className="flex gap-10 items-center">
-                <Component />
+                <DeleteComputer />
             </div>
         </div>
     )
