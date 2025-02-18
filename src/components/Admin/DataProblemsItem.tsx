44a265cc9a4a8fee806cef031c35b48adcdfdad7
Mehedov <mehedov.dev@yandex.ru>
2025-02-18 12:47:32 +0300

    I've changed the styles and structure of the table fields a bit.

--- src/components/Admin/DataProblemsItem.tsx
+++ src/components/Admin/DataProblemsItem.tsx
@@ -1,21 +1,25 @@
+import { ChangeStatusSelect } from '@/components/ChangeStatusSelect.tsx'
 import { FC } from 'react'
 import { TComputersInProblem } from './DataProblems'
 
-type TDataProblemsItemProps = Omit<TComputersInProblem, 'status'>
+type TDataProblemsItemProps = TComputersInProblem
 
 const DataProblemsItem: FC<TDataProblemsItemProps> = ({
     message,
     id,
     auditorium,
+    status,
 }) => {
     return (
-        <div className="flex items-center dark:border-neutral-700 border-b-[1px] p-3">
-            <div className="w-[33%]">{message.author}</div>
-            <div className="w-[33%] flex items-center gap-1">
+        <div className="flex items-center gap-5 dark:border-neutral-700 border-b-[1px] p-3 ">
+            <div className="w-[15%] flex items-center gap-1">
                 <div>№{id}</div>
                 <span>ауд. {auditorium}</span>
             </div>
-            <div className="w-[33%]">{message.text}</div>
+            <div className="w-[60%] break-words">{message}</div>
+            <div className="w-[15%]">
+                <ChangeStatusSelect status={status} />
+            </div>
         </div>
     )
 }
