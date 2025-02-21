6e00ffe4dff57c2af8092767382dbf37566e4615
Mehedov <mehedov.dev@yandex.ru>
2025-02-21 11:55:51 +0300

    Adding props to ChangeStatusSelect

--- src/components/Admin/DataProblemsItem.tsx
+++ src/components/Admin/DataProblemsItem.tsx
@@ -1,5 +1,7 @@
 import { ChangeStatusSelect } from '@/components/ChangeStatusSelect.tsx'
-import { FC } from 'react'
+import { setComputerStatus } from '@/redux/slices/computer/slice'
+import { useAppDispatch } from '@/redux/store'
+import { FC, useEffect, useState } from 'react'
 import { TComputersInProblem } from './DataProblems'
 
 type TDataProblemsItemProps = TComputersInProblem
@@ -10,6 +12,15 @@ const DataProblemsItem: FC<TDataProblemsItemProps> = ({
     auditorium,
     status,
 }) => {
+    const dispatch = useAppDispatch()
+    const [currentStatus, setCurrentStatus] = useState('')
+
+    useEffect(() => {
+        if (id && currentStatus) {
+            dispatch(setComputerStatus({ id, status: currentStatus }))
+        }
+    }, [currentStatus])
+
     return (
         <div className="flex items-center gap-5 dark:border-neutral-700 border-b-[1px] p-3 ">
             <div className="w-[18%] flex items-center gap-1">
@@ -18,7 +29,11 @@ const DataProblemsItem: FC<TDataProblemsItemProps> = ({
             </div>
             <div className="w-[60%] break-words">{message}</div>
             <div className="w-[12%]">
-                <ChangeStatusSelect status={status} />
+                <ChangeStatusSelect
+                    currentStatus={currentStatus}
+                    setCurrentStatus={setCurrentStatus}
+                    status={status}
+                />
             </div>
         </div>
     )
