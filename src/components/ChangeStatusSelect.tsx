8785dc2d5cd6e949e40f1289b2395f091ae155b4
Mehedov <mehedov.dev@yandex.ru>
2025-02-21 11:57:41 +0300

    Implemented select status switching

--- src/components/ChangeStatusSelect.tsx
+++ src/components/ChangeStatusSelect.tsx
@@ -1,41 +1,108 @@
+import { Button } from '@/components/ui/button'
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select'
-import { STATUSES } from '@/constants.ts'
+    Command,
+    CommandGroup,
+    CommandItem,
+    CommandList,
+} from '@/components/ui/command'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover'
+import { COLORS, STATUSES } from '@/constants'
+import { cn } from '@/lib/utils'
+import { Check, ChevronsUpDown } from 'lucide-react'
+import { Dispatch, useState } from 'react'
 
-export function ChangeStatusSelect({ status }: { status: string }) {
+const statuses = [
+    {
+        value: STATUSES.work,
+        label: STATUSES.work,
+        color: COLORS.green,
+    },
+    {
+        value: STATUSES.notWork,
+        label: STATUSES.notWork,
+        color: COLORS.red,
+    },
+    {
+        value: STATUSES.process,
+        label: STATUSES.process,
+        color: COLORS.yellow,
+    },
+]
+
+type TChangeStatusSelectProps = {
+    status: string
+    currentStatus: string
+    setCurrentStatus: Dispatch<React.SetStateAction<string>>
+}
+
+export const ChangeStatusSelect = ({
+    status,
+    currentStatus,
+    setCurrentStatus,
+}: TChangeStatusSelectProps) => {
+    const [open, setOpen] = useState(false)
+
+    const statusColor = statuses.find(
+        (currentStatus) => currentStatus.value === status
+    )
+
     return (
-        <Select>
-            <SelectTrigger className="w-[130px]">
-                <SelectValue placeholder={status} />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    <SelectItem
-                        className=" text-[#06AB46]"
-                        value={STATUSES.work}
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[130px] justify-between"
+                >
+                    <span
+                        className={cn(
+                            `text-[${statusColor ? statusColor.color : ''}]`
+                        )}
                     >
-                        {STATUSES.work}
-                    </SelectItem>
-                    <SelectItem
-                        className="text-[hsl(var(--destructive))]"
-                        value={STATUSES.notWork}
-                    >
-                        {STATUSES.notWork}
-                    </SelectItem>
-                    <SelectItem
-                        className="text-[#f0aa48]"
-                        value={STATUSES.process}
-                    >
-                        {STATUSES.process}
-                    </SelectItem>
-                </SelectGroup>
-            </SelectContent>
-        </Select>
+                        {currentStatus !== '' ? currentStatus : status}
+                    </span>
+
+                    <ChevronsUpDown className="opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandList>
+                        <CommandGroup>
+                            {statuses.map((status) => (
+                                <CommandItem
+                                    key={status.value}
+                                    value={status.value}
+                                    className={cn(`text-[${status.color}]`)}
+                                    onSelect={(currentValue) => {
+                                        setCurrentStatus(
+                                            currentValue === currentStatus
+                                                ? ''
+                                                : currentValue
+                                        )
+                                        setOpen(false)
+                                    }}
+                                >
+                                    {status.label}
+                                    <Check
+                                        className={cn(
+                                            'ml-auto',
+                                            currentStatus === status.value
+                                                ? 'opacity-100'
+                                                : 'opacity-0'
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
     )
 }
