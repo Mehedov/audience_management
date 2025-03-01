62a292311e9b75ed6e3e9877de9248fcf0868e42
Mehedov <mehedov.dev@yandex.ru>
2025-03-01 22:42:10 +0300

    filter.utils.ts: Adding error handling

--- src/utils/filter.utils.ts
+++ src/utils/filter.utils.ts
@@ -1,22 +1,24 @@
-import {IComputerData} from '@/redux/slices/computer/slice'
-import {MutableRefObject} from 'react'
+import { TAudienceSearchValues } from '@/components/Control/ControlAudienceSearch'
+import { IComputerData } from '@/redux/slices/computer/slice'
+import { UseFormSetError } from 'react-hook-form'
 
 export const filterByAuditorium = (
     aud: string,
     computers: IComputerData[],
     callback: (newComputers: IComputerData[]) => void,
-    error?: (value: boolean) => void,
-    textError?: MutableRefObject<string>
+    setError: UseFormSetError<TAudienceSearchValues>
 ) => {
     const newComputers = [...computers].filter((obj) => obj.auditorium === aud)
     if (newComputers.length) {
         callback(newComputers)
         return newComputers
     } else {
-        if (error && textError) {
-            error(true)
-            textError.current = 'Аудитория не найдена.'
+        setError('aud', { type: 'manual', message: 'Аудитория не найдена' })
+        if (aud.length > 4) {
+            setError('aud', {
+                type: 'manual',
+                message: 'Максимальное допустимое значение 4 символа',
+            })
         }
-        throw new Error('Аудитория не найдена.')
     }
 }
