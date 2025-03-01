ebdad04760df635b53632866df33732a66320821
Mehedov <mehedov.dev@yandex.ru>
2025-03-01 23:09:31 +0300

    Removed unnecessary imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
