c42466421a1c1a15f181db42a16efa6d773db0b0
Mehedov <mehedov.dev@yandex.ru>
2025-03-01 22:44:03 +0300

    Reduced unnecessary logic

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -1,5 +1,5 @@
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { MutableRefObject, useRef, useState } from 'react'
+import { MutableRefObject } from 'react'
 import { ControlAudienceSearch } from './ControlAudienceSearch'
 import { ControlProblemMessage } from './ControlProblemMessage'
 
@@ -12,40 +12,15 @@ export type TControlValidateElements = {
     textSuccess: MutableRefObject<string>
 }
 
-/**
- * TODO:
- *      - Возможно нужно будет кинуть все эти стейти в redux или глянуть
- *      - Использование какой нибудь библиотеки для валидации данных
- */
-
 export function Control() {
-    const [isError, setIsError] = useState(false)
-    const [isSuccess, setIsSuccess] = useState(false)
-    const textError = useRef('Все поля должны быть заполнены.')
-    const textSuccess = useRef('Ваше сообщение отправлено.')
-
     return (
         <Tabs defaultValue="Аудитория" className="max-[600">
             <TabsList className="grid w-full grid-cols-2">
                 <TabsTrigger value="Аудитория">Аудитория</TabsTrigger>
                 <TabsTrigger value="Компьютер">Компьютер</TabsTrigger>
             </TabsList>
-            <ControlAudienceSearch
-                isError={isError}
-                setIsError={setIsError}
-                setIsSuccess={setIsSuccess}
-                isSuccess={isSuccess}
-                textError={textError}
-                textSuccess={textSuccess}
-            />
-            <ControlProblemMessage
-                isError={isError}
-                setIsError={setIsError}
-                setIsSuccess={setIsSuccess}
-                isSuccess={isSuccess}
-                textError={textError}
-                textSuccess={textSuccess}
-            />
+            <ControlAudienceSearch />
+            <ControlProblemMessage />
         </Tabs>
     )
 }
