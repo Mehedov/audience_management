7b8101232d2ec1a6cfb55ef1da8a1164a21664a6
Mehedov <mehedov.dev@yandex.ru>
2025-03-03 00:06:08 +0300

    Added implemented logic for working with forms to the component

--- src/components/Admin/AddingComputer.tsx
+++ src/components/Admin/AddingComputer.tsx
@@ -6,58 +6,58 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import useSuccessMessage from '@/hooks/useSuccessMessage'
 import { selectorComputerItems } from '@/redux/slices/computer/selections'
 import { IComputerData, setComputers } from '@/redux/slices/computer/slice'
 import { useAppDispatch, useAppSelector } from '@/redux/store'
-import { FormEvent, useRef, useState } from 'react'
-import {ValidationComponent} from '../ValidationComponent'
+import { findComputerById } from '@/utils/find.utils'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Input } from '../Input'
+import ToastWrapper from '../Toast/ToastWrapper'
+
+type TAddingComputerValues = Omit<IComputerData, 'message'>
 
 export const AddingComputer = () => {
     const dispatch = useAppDispatch()
     const computers = useAppSelector(selectorComputerItems)
-    const [isError, setIsError] = useState(false)
-    const [isSuccess, setIsSuccess] = useState(false)
-    const [computer, setComputer] = useState<IComputerData>({
-        id: '',
-        status: '',
-        auditorium: '',
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitSuccessful },
+        setError,
+    } = useForm<TAddingComputerValues>({
+        defaultValues: {
+            id: '',
+            status: '',
+            auditorium: '',
+        },
     })
-    const errorMessage = useRef('')
+    const { showSuccessMessage, triggerSuccessMessage } = useSuccessMessage()
 
-    const newComputerAdd = (event: FormEvent, newComputer: IComputerData) => {
-        event.preventDefault()
-        const thereId = computers.find((obj) => obj.id === computer.id)
-        if (newComputer && !thereId) {
-            dispatch(setComputers([...computers, newComputer]))
-            setComputer({
-                id: '',
-                status: '',
-                auditorium: '',
-            })
-            setIsSuccess(true)
-        } else {
-            errorMessage.current = 'Такой компьютер уже есть.'
-            setIsError(true)
-        }
-        return setTimeout(() => {
-            setIsSuccess(false)
-        }, 1000)
-    }
+    const newComputerAdd: SubmitHandler<TAddingComputerValues> = (
+        data,
+        event
+    ) => {
+        event?.preventDefault()
 
-    const validateProblemMessage = () => {
-        if (
-            computer.id === '' ||
-            computer.status === '' ||
-            computer.auditorium === ''
-        ) {
-            errorMessage.current = 'Все поля должны быть заполнены.'
-            setIsError(true)
-        }
-        return setTimeout(() => {
-            setIsError(false)
-        }, 1000)
+        triggerSuccessMessage(() => {
+            if (data.id.length > 4) {
+                setError('id', {
+                    type: 'manual',
+                    message: 'Максимальное допустимое значение 4 символа',
+                })
+            }
+            const there = findComputerById(data.id, computers)
+            if (!there) {
+                dispatch(setComputers([...computers, data]))
+            } else {
+                setError('id', {
+                    type: 'manual',
+                    message: 'Этот компьютер уже есть',
+                })
+            }
+        })
     }
 
     return (
@@ -66,72 +66,39 @@ export const AddingComputer = () => {
                 <CardTitle>Добавление компьютера</CardTitle>
             </CardHeader>
             <CardContent>
-                <form onSubmit={(e) => newComputerAdd(e, computer)}>
+                <form onSubmit={handleSubmit(newComputerAdd)}>
                     <CardContent className="space-y-2">
                         <div className="space-y-1">
                             <Label htmlFor="id">Номер компьютера</Label>
-                            <Input
-                                value={computer.id}
-                                onChange={(e) =>
-                                    setComputer({
-                                        ...computer,
-                                        id: e.target.value,
-                                    })
-                                }
-                                required
-                                id="id"
-                                maxLength={4}
-                                type="text"
-                            />
+                            <Input name="id" register={register} required />
                         </div>
                         <div className="space-y-1">
                             <Label htmlFor="status">
                                 Статус компьютера(Рабочий, не рабочий, в
                                 процессе)
                             </Label>
-                            <Input
-                                value={computer.status}
-                                onChange={(e) =>
-                                    setComputer({
-                                        ...computer,
-                                        status: e.target.value,
-                                    })
-                                }
-                                required
-                                id="status"
-                                type="text"
-                            />
+                            <Input name="status" register={register} required />
                         </div>
                         <div className="space-y-1">
                             <Label htmlFor="auditorium">Номер аудитории</Label>
+
                             <Input
-                                value={computer.auditorium}
-                                onChange={(e) =>
-                                    setComputer((prev) => ({
-                                        ...prev,
-                                        auditorium: e.target.value,
-                                    }))
-                                }
+                                name="auditorium"
+                                register={register}
                                 required
-                                id="auditorium"
-                                type="text"
                             />
                         </div>
-                        <ValidationComponent
-                            isError={isError}
-                            isSuccess={isSuccess}
-                            textSuccess="Компьютер добавлен."
-                            textError={errorMessage.current}
+                        <ToastWrapper
+                            errors={errors}
+                            isSubmitSuccessful={isSubmitSuccessful}
+                            showSuccessMessage={showSuccessMessage}
                         />
                     </CardContent>
                     <CardFooter>
-                        <Button onClick={validateProblemMessage}>
-                            Добавить
-                        </Button>
+                        <Button>Добавить</Button>
                     </CardFooter>
                 </form>
             </CardContent>
         </Card>
     )
 }
-
--- src/components/Admin/DeleteComputer.tsx
+++ src/components/Admin/DeleteComputer.tsx
@@ -6,49 +6,45 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import useSuccessMessage from '@/hooks/useSuccessMessage'
 import { selectorComputerItems } from '@/redux/slices/computer/selections'
 import { setComputers } from '@/redux/slices/computer/slice'
 import { useAppDispatch, useAppSelector } from '@/redux/store'
-import { FormEvent, useRef, useState } from 'react'
-import { ValidationComponent } from '../ValidationComponent'
+import { findComputerById } from '@/utils/find.utils'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Input } from '../Input'
+import ToastWrapper from '../Toast/ToastWrapper'
+
+type TDeleteComputerValues = {
+    id: string
+}
 
 export const DeleteComputer = () => {
     const dispatch = useAppDispatch()
     const computers = useAppSelector(selectorComputerItems)
-    const [isError, setIsError] = useState(false)
-    const [isSuccess, setIsSuccess] = useState(false)
-    const [id, setId] = useState('')
-    const errorMessage = useRef('')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitSuccessful },
+        setError,
+    } = useForm<TDeleteComputerValues>({
+        defaultValues: {
+            id: '',
+        },
+    })
+    const { showSuccessMessage, triggerSuccessMessage } = useSuccessMessage()
 
-
-    const removeComputer = (event: FormEvent, id:string) => {
-        event.preventDefault()
-        const newComputers = computers.filter((obj) => obj.id !== id)
-		const isComputer = computers.find((obj) => obj.id === id)
-        if (newComputers && isComputer) {
-            dispatch(setComputers(newComputers))
-            setIsSuccess(true)
-        } else {
-            errorMessage.current = 'Данный компьютер не найден.'
-            setIsError(true)
-        }
-        return setTimeout(() => {
-            setIsSuccess(false)
-        }, 1000)
-    }
-
-    const validateProblemMessage = () => {
-        if (
-            id === ''
-        ) {
-            errorMessage.current = 'Все поля должны быть заполнены.'
-            setIsError(true)
-        }
-        return setTimeout(() => {
-            setIsError(false)
-        }, 1000)
+    const removeComputer: SubmitHandler<TDeleteComputerValues> = (
+        data,
+        event
+    ) => {
+        event?.preventDefault()
+        triggerSuccessMessage(() => {
+            const newComputers = computers.filter((obj) => obj.id !== data.id)
+            findComputerById(data.id, computers, setError)
+            newComputers && dispatch(setComputers(newComputers))
+        })
     }
 
     return (
@@ -57,32 +53,20 @@ export const DeleteComputer = () => {
                 <CardTitle>Удаление компьютера</CardTitle>
             </CardHeader>
             <CardContent>
-                <form onSubmit={(e) => removeComputer(e, id)}>
+                <form onSubmit={handleSubmit(removeComputer)}>
                     <CardContent className="space-y-2">
                         <div className="space-y-1">
                             <Label htmlFor="id">Номер компьютера</Label>
-                            <Input
-                                value={id}
-                                onChange={(e) =>
-                                    setId(e.target.value)
-                                }
-                                required
-                                id="id"
-                                maxLength={4}
-                                type="text"
-                            />
+                            <Input name="id" register={register} required />
                         </div>
-                        <ValidationComponent
-                            isError={isError}
-                            isSuccess={isSuccess}
-                            textSuccess="Компьютер удален."
-                            textError={errorMessage.current}
+                        <ToastWrapper
+                            errors={errors}
+                            isSubmitSuccessful={isSubmitSuccessful}
+                            showSuccessMessage={showSuccessMessage}
                         />
                     </CardContent>
                     <CardFooter>
-                        <Button onClick={validateProblemMessage}>
-                            Удалить
-                        </Button>
+                        <Button>Удалить</Button>
                     </CardFooter>
                 </form>
             </CardContent>
