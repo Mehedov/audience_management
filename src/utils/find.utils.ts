3b1d370707db9cbaa9ecb66d4be33bf9bf855550
Mehedov <mehedov.dev@yandex.ru>
2025-03-03 00:01:55 +0300

    Change in error handling

--- src/utils/find.utils.ts
+++ src/utils/find.utils.ts
@@ -4,18 +4,19 @@ import { UseFormSetError } from 'react-hook-form'
 export const findComputerById = (
     id: string,
     computers: IComputerData[],
-    setError: UseFormSetError<{ id: string }>
+    setError?: UseFormSetError<{ id: string }>
 ) => {
     const find = computers.find((comp) => comp.id === id)
     if (find) {
         return find
     } else {
-        setError('id', { type: 'manual', message: 'Компьютер не найден' })
-        if (id.length > 4) {
-            setError('id', {
-                type: 'manual',
-                message: 'Максимальное допустимое значение 4 символа',
-            })
+        if (setError) {
+            setError('id', { type: 'manual', message: 'Компьютер не найден' })
+            id.length > 4 &&
+                setError('id', {
+                    type: 'manual',
+                    message: 'Максимальное допустимое значение 4 символа',
+                })
         }
     }
 }
