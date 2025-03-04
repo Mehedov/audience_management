1f9c65060dd5c880fda0a29dc6ad9da8138596fe
Mehedov <mehedov.dev@yandex.ru>
2025-03-04 14:30:08 +0300

    Added a button to save the changed computer status to the table objects

--- src/components/Admin/DataProblemsItem.tsx
+++ src/components/Admin/DataProblemsItem.tsx
@@ -1,7 +1,10 @@
+import applySvg from '@/assets/icons/apply.svg'
+import saveSvg from '@/assets/icons/save.svg'
 import { ChangeStatusSelect } from '@/components/ChangeStatusSelect.tsx'
 import { setComputerStatus } from '@/redux/slices/computer/slice'
 import { useAppDispatch } from '@/redux/store'
 import { FC, useEffect, useState } from 'react'
+import { Button } from '../ui/button'
 import { TComputersInProblem } from './DataProblems'
 
 type TDataProblemsItemProps = TComputersInProblem
@@ -14,26 +17,53 @@ const DataProblemsItem: FC<TDataProblemsItemProps> = ({
 }) => {
     const dispatch = useAppDispatch()
     const [currentStatus, setCurrentStatus] = useState('')
+    const [apply, setApply] = useState(false)
 
-    useEffect(() => {
+    const changeStatus = () => {
         if (id && currentStatus) {
             dispatch(setComputerStatus({ id, status: currentStatus }))
+            setApply(status === currentStatus)
         }
-    }, [currentStatus])
+    }
+
+    useEffect(() => {
+        setApply(status === currentStatus)
+    }, [status, currentStatus])
 
     return (
         <div className="flex items-center gap-5 dark:border-neutral-700 border-b-[1px] p-3 ">
             <div className="w-[18%] flex items-center gap-1">
-                <div>№{id}</div>
-                <span>ауд. {auditorium}</span>
+                <div>
+                    № <b>{id}</b>
+                </div>
+                <span>
+                    ауд. <b>{auditorium}</b>
+                </span>
             </div>
-            <div className="w-[60%] break-words">{message}</div>
-            <div className="w-[12%]">
-                <ChangeStatusSelect
-                    currentStatus={currentStatus}
-                    setCurrentStatus={setCurrentStatus}
-                    status={status}
-                />
+            <div className="w-[50%] break-words">
+                <b>{message}</b>
+            </div>
+            <div className="w-[22%] flex items-center justify-between">
+                <div className="mr-3">
+                    <ChangeStatusSelect
+                        currentStatus={currentStatus}
+                        setCurrentStatus={setCurrentStatus}
+                        status={status}
+                    />
+                </div>
+                <div>
+                    <Button
+                        className="flex justify-center items-center"
+                        variant="outline"
+                        onClick={changeStatus}
+                    >
+                        {apply ? (
+                            <img src={applySvg} width={20} />
+                        ) : (
+                            <img src={saveSvg} width={25} className="w-6" />
+                        )}
+                    </Button>
+                </div>
             </div>
         </div>
     )
