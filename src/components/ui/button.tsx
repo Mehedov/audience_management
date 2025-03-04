210d851bf94b0ebdde03aaf7035721a045b04bc7
Mehedov <mehedov.dev@yandex.ru>
2025-03-04 14:28:05 +0300

    Added children to the button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,14 +43,19 @@ export interface ButtonProps
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
+    (
+        { className, variant, size, asChild = false, children, ...props },
+        ref
+    ) => {
         const Comp = asChild ? Slot : 'button'
         return (
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
                 ref={ref}
                 {...props}
-            />
+            >
+                {children}
+            </Comp>
         )
     }
 )
